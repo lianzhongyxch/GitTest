bc30c1de602f253b236f82dad93f2f3c138a0604
lianzhongyxch <lianzhongyxch@163.com>
2014-08-14 00:10:28 +0800

    init project version

---
+++ MyGitTest/MyGitTest.cpp
@@ -0,0 +1,14 @@
+// MyGitTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	printf("---------------------\r\n");
+	printf("My GIT Test\r\n");
+	printf("Hello World!\n");
+	printf("=====================\r\n");
+	return 0;
+}
+
